928e5e7d2e284757130288dd88bec88621033fac
Jason <baofeng_zheng@163.com>
2018-01-13 17:04:36 +0800

    完成功能

--- src/Mommifier.java
+++ src/Mommifier.java
@@ -1,2 +1,40 @@
+import java.util.Vector;
+
 public class Mommifier {
+
+    public static void main()
+    {
+        mommy("abb");
+    }
+    public static String mommy(String inputStr) {
+        String res = "";
+        boolean isPrevCharacterVowel = false;
+        int n = 0;
+        float length = inputStr.length();
+        for (int i = 0; i < length; ++i) {
+            String subStr = inputStr.substring(i, i + 1);
+            if (isVowel(subStr.toLowerCase())) {
+                ++n;
+                if (isPrevCharacterVowel==false)
+                    res += "mommy";
+                else
+                    res += subStr;
+                isPrevCharacterVowel = true;
+                continue;
+            }
+            isPrevCharacterVowel = false;
+            res+=subStr;
+        }
+        if (n > length * 0.3)
+            return res;
+        return inputStr;
+    }
+
+    public static boolean isVowel(String str) {
+        if (str.equals("a")||str.equals("e")||str.equals("i")||str.equals("o")||str.equals("u"))
+            return true;
+        return false;
+    }
+
+
 }
--- test/MommifierTest.java
+++ test/MommifierTest.java
@@ -1,9 +1,29 @@
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class MommifierTest {
 
     @Test
-    public void shouldReturnEmptyWhenInputIsEmpty(){
+    public void shouldReturnEmptyWhenInputIsEmpty() {
+        Mommifier mommifier = new Mommifier();
+        String result;
+        result = mommifier.mommy("");
+        assertThat(result, is(""));
+    }
 
+    @Test
+    public void shouldReturnMommyWhenInputIsVowel() {
+        Mommifier mommifier = new Mommifier();
+        String result = mommifier.mommy("abaa");
+        assertThat(result, is("mommybmommya"));
+    }
+
+    @Test
+    public void shoudReturnTrueWhenInputIsVowel(){
+        Mommifier mommifier = new Mommifier();
+        boolean result = mommifier.isVowel("a");
+        assertThat(result, is(true));
     }
 }
